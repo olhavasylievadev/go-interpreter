aff2b975673e6bd5d6b3c2cf633e995b4a273c90
olhavasylievadev <106344802+olhavasylievadev@users.noreply.github.com>
2023-11-08 15:17:15 +0100

    Initial commit

---
+++ token/token.go
@@ -0,0 +1,45 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN   = "ASSIGN"
+	MINUS    = "-"
+	DIVIDE   = "/"
+	MULTIPLY = "*"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
